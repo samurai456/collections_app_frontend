2153a5cb33ee82ed0826445bb0a91a7e292c7aad
samurai456 <ula.sadikov2001@gmail.com>
2023-07-20 16:27:30 +0500

    'f'

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -56,8 +56,9 @@ function Item(){
 
     
     function getComments(res){
+        if(!res.comments) return
         flushSync(()=>{
-            setComments(res.comments||[])
+            setComments(res.comments)
         })
         const foundComment = document.getElementById(commentId.current)
         if(commentId.current && foundComment){
